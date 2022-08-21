c44c8d92fb5b1bec54c6c2f8785e1df2f7decb7f
Hari Vigneshwaran <96340194+coderhariexpert@users.noreply.github.com>
2022-08-21 11:44:56 +0530

    Done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -50,9 +50,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="https://www.linkedin.com/company/google/"><img src={navIcon1} alt="" /></a>
-                <a href="https://www.facebook.com/Google/"><img src={navIcon2} alt="" /></a>
-                <a href="https://www.instagram.com/anormalguy652/"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.linkedin.com/company/google/" target="_blank"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.facebook.com/Google/" target="_blank"><img src={navIcon2} alt="" /></a>
+                <a href="https://www.instagram.com/anormalguy652/" target="_blank"><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
